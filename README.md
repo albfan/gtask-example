0bedf92d56a9cb8e1d205e9cf46c310c24c9fb5a
albfan <albertofanjul@gmail.com>
2017-08-23 12:30:38 +0200

    Block version

---
+++ README.md
@@ -0,0 +1,21 @@
+# gtask-example
+
+This is the reconstruction of a question and answer from stackoverflow
+
+https://stackoverflow.com/questions/36971139/gui-becomes-unresponsive-after-clicking-the-button-using-gtk-in-c
+
+# Usage
+
+This can be run from command line
+
+    $ git clone <this-repo>
+    $ cd <this-repo>
+    $ mkdir build
+    $ cd build
+    $ meson ..
+    $ ninja
+    $ sudo ninja install
+    $ gtask-example
+
+or with a GUI IDE (like [gnome-builder](https://wiki.gnome.org/Apps/Builder))
+
---
+++ src/gtask-example-window.c
@@ -0,0 +1,58 @@
+/* gtask-example-window.c
+ *
+ * Copyright (C) 2017 albfan
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <gtk/gtk.h>
+#include "gtask-example-config.h"
+#include "gtask-example-window.h"
+
+struct _GtaskExampleWindow
+{
+  GtkWindow     parent_instance;
+  GtkHeaderBar *header_bar;
+  GtkLabel     *label;
+  GtkButton    *button;
+};
+
+G_DEFINE_TYPE (GtaskExampleWindow, gtask_example_window, GTK_TYPE_APPLICATION_WINDOW)
+
+void display(GtkButton * b, gpointer data)
+{
+  while(1)
+    {
+      printf("hurrah!\n");
+    }
+}
+
+static void
+gtask_example_window_class_init (GtaskExampleWindowClass *klass)
+{
+  GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
+
+  gtk_widget_class_set_template_from_resource (widget_class, "/org/gnome/Gtask-Example/gtask-example-window.ui");
+  gtk_widget_class_bind_template_child (widget_class, GtaskExampleWindow, label);
+  gtk_widget_class_bind_template_child (widget_class, GtaskExampleWindow, button);
+}
+
+static void
+gtask_example_window_init (GtaskExampleWindow *self)
+{
+  gtk_widget_init_template (GTK_WIDGET (self));
+  g_signal_connect (self->button, "clicked", G_CALLBACK (display), NULL);
+
+}
+
